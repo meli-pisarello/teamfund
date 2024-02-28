d82ca725f187a1bd668a15d39bdc5e390eb60d01
meli <mepisarello@gmail.com>
2024-02-28 12:26:51 -0300

    BE initial setup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 5000;
+const PORT = 4081;
+
+app.get('/', (req, res) => {
+    res.end('Node backend server');
+})
 
 app.listen(PORT, function() {
-    console.log(`The server is running on port: ${PORT}`);
+    console.log(`The server is running on port: ${PORT}!`);
 });
