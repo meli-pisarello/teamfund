217685a6e4a7a4e31c370c9b31bd0d92e7e7df4c
meli <mepisarello@gmail.com>
2024-02-28 11:41:22 -0300

    Added react router and server

--- fe-teamfund/src/App.js
+++ fe-teamfund/src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+import SalesIndex from './pages/sales';
+import ErrorPage from './pages/error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (<h1>This is the Home page</h1>),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/sales',
+    element: <SalesIndex />,
+    errorElement: <ErrorPage />,
+    exact: true
+  }
+]);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <RouterProvider router={router} />
       </header>
     </div>
   );
---
+++ fe-teamfund/src/pages/sales.jsx
@@ -0,0 +1,7 @@
+function SalesIndex () {
+    return (
+        <h1>This is the list of sales</h1>
+    );
+}
+
+export default SalesIndex;
